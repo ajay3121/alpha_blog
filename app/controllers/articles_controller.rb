be5ac8c609b6db996381917fea9c31ef07057426
Ajay <maliajay1611@gmail.com>
2023-09-14 16:20:59 +0530

    many changes are made

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,33 @@
+class ArticlesController < ApplicationController
+    def show
+        @article = Article.find(params[:id])
+    end
+    def index
+        @article = Article.all
+    end
+    def new
+        @article = Article.new
+    end
+    def create 
+        @article = Article.new(params.require(:article).permit(:title,:description))
+        if @article.save
+            redirect_to (@article)
+        else 
+            render 'new'
+        end
+    end
+    def edit
+        @article = Article.find(params[:id])
+    end
+    def update
+        @article = Article.find(params[:id])
+        @article.update(params.require(:article).permit(:title,:description))
+        redirect_to articles_path
+    end
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+    end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   root 'index#home'
   get 'about', to: 'index#about'
   get 'signin', to: 'page_1#fpage'
+  resources :articles
 end
